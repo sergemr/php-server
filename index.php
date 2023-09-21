013c2f5fac20bc25fb5189f8720eed6f0ae3167c
sergemr <sergemr@gmail.com>
2023-09-21 09:09:35 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to PHP</title>
+</head>
+<body>
+    <h1>Welcome to PHP</h1>
+
+    <?php
+    // Define variables
+    $name = "John";
+    $age = 30;
+
+    // Use variables in PHP
+    echo "<p>Hello, my name is $name.</p>";
+    echo "<p>I am $age years old.</p>";
+
+    // Perform a simple calculation
+    $x = 5;
+    $y = 3;
+    $sum = $x + $y;
+
+    echo "<p>$x + $y = $sum</p>";
+    ?>
+
+    <p>This is a simple PHP page.</p>
+</body>
+</html>
